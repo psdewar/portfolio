5d4aa2f334c6fd8bdf33158207f96fe3686b20c2
Peyt Spencer Dewar <psdewar2@live.com>
2024-07-03 19:22:02 -0700

    Updated sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -5,6 +5,10 @@ export default async function sitemap() {
       lastModified: new Date("03/26/2018").toISOString().split("T")[0],
     },
     {
+      url: "https://youtube.com/peytspencer",
+      lastModified: new Date().toISOString().split("T")[0],
+    },
+    {
       url: "https://youtu.be/es609on1vZI",
       lastModified: new Date("02/04/2024").toISOString().split("T")[0],
     },
@@ -15,6 +19,10 @@ export default async function sitemap() {
     {
       url: "https://youtu.be/tZrm30Qk6xI",
       lastModified: new Date("04/24/2020").toISOString().split("T")[0],
+    },
+    {
+      url: "https://www.instagram.com/peytspencer",
+      lastModified: new Date().toISOString().split("T")[0],
     },
     {
       url: "https://www.instagram.com/p/B_NZXQsF-Vo",
